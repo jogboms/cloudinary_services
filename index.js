aebcf7558f866f590edcc5a952a07ab4a23b6864
Afees Lawal <moderateepheezy@gmail.com>
2018-04-26 15:11:59 +0100

    feat: Integrated Cloudinary

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const json = data => JSON.stringify(data);
+var cloudinary = require("cloudinary");
+var config = require("config");
 
 module.exports = (req, res) => {
-  res.end(
-    json({
-      title: "Welcome to Micro"
-    })
-  );
+  cloudinary.config(config);
+
+  const promise = new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
+      function(result) {
+        console.log(result);
+        resolve(result);
+      },
+      { public_id: "newformattedId" }
+    );
+  });
+
+  res.end(json(promise));
 };
