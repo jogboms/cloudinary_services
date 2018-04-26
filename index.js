f85bf86c8b1be5eb12831e0ccf827f46fd165a82
Jogboms <jeremiahogbomo@gmail.com>
2018-04-26 16:02:13 +0100

    refactor: code clean up

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
+const { uploader, config } = require("cloudinary");
 const json = data => JSON.stringify(data);
-var cloudinary = require("cloudinary");
-var config = require("config");
 
-module.exports = (req, res) => {
-  cloudinary.config(config);
+config(require("./config"));
 
-  const promise = new Promise((resolve, reject) => {
-    cloudinary.uploader.upload(
+module.exports = async (req, res) => {
+  const results = await new Promise((resolve, reject) => {
+    uploader.upload(
       "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
-      function(result) {
-        console.log(result);
-        resolve(result);
-      },
+      resolve,
       { public_id: "newformattedId" }
     );
   });
-
-  res.end(json(promise));
+  res.end(json(results));
 };
