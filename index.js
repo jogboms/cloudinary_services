54fa85bf97f5a66f7f9069748ebae6a8bfe55bae
Jogboms <jeremiahogbomo@gmail.com>
2018-04-27 10:01:28 +0100

    revert: convert to ES6 module cos of tests

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import { uploader, config } from "cloudinary";
+const { uploader, config } = require("cloudinary");
 const json = data => JSON.stringify(data);
 
 config(require("./config"));
 
-export default async function(req, res) {
+module.exports = async (req, res) => {
   const results = await new Promise((resolve, reject) => {
     uploader.upload(
       "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
-      // "https://images.pexels.com",
       response => {
         if (response && response.error) {
           return reject(response.error);
@@ -18,4 +17,4 @@ export default async function(req, res) {
     );
   });
   res.end(json(results));
-}
+};
