8cc3b82a10e6e50286bc9f7132fad97240f60f92
Jogboms <jeremiahogbomo@gmail.com>
2018-04-27 14:30:24 +0100

    chore(test): added more tests

--- test.js
+++ test.js
@@ -4,30 +4,44 @@ import listen from "test-listen";
 import request from "request-promise";
 import app from ".";
 
-test("init tests", async t => {
+test("test w/o url", async t => {
   const service = micro(app);
+  const uri = await listen(service);
+  const body = await request({
+    method: "POST",
+    uri: uri
+  });
+  t.deepEqual(JSON.parse(body), { statusCode: 400 });
+  service.close();
+});
 
-  const body = await request(await listen(service));
-  const keys = [
-    "public_id",
-    "version",
-    "signature",
-    "width",
-    "height",
-    "format",
-    "resource_type",
-    "created_at",
-    "tags",
-    "bytes",
-    "type",
-    "etag",
-    "placeholder",
-    "url",
-    "secure_url",
-    "overwritten",
-    "original_filename",
-    "original_extension"
-  ];
-  t.deepEqual(Object.keys(JSON.parse(body)), keys, "Hurray! Legooo!");
+test("test w/ url", async t => {
+  const service = micro(app);
+  const uri = await listen(service);
+  const body = await request({
+    method: "POST",
+    uri: uri,
+    body: JSON.stringify({
+      url:
+        "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350"
+    })
+  });
+  t.deepEqual(Object.keys(JSON.parse(body)), ["url", "public_id"]);
+  service.close();
+});
+
+test("test w/ url & public_id", async t => {
+  const service = micro(app);
+  const uri = await listen(service);
+  const body = await request({
+    method: "POST",
+    uri: uri,
+    body: JSON.stringify({
+      url:
+        "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
+      public_id: "PUBLIC_ID"
+    })
+  });
+  t.deepEqual(Object.keys(JSON.parse(body)), ["url", "public_id"]);
   service.close();
 });
