73a151805327df5acb481367b79a7bf7f08cb793
Jogboms <jeremiahogbomo@gmail.com>
2018-04-27 14:31:52 +0100

    refactor: added public_id to public api

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const { uploader, config } = require("cloudinary");
 const jsonRes = data => JSON.stringify(data);
-const { json } = require("micro");
+const { json, createError } = require("micro");
 
 config(require("./config"));
 
 module.exports = async (req, res) => {
-  const data = req && (await json(req));
-  console.log(data);
-  const results = await new Promise((resolve, reject) => {
-    uploader.upload(
-      data.url,
-      response => {
-        if (response && response.error) {
-          return reject(response.error);
-        }
-        resolve(response);
-      },
-      data.public_id ? { public_id: data.public_id } : void 0
-    );
-  });
-  res.end(jsonRes({ url: results.url }));
+  try {
+    const data = await json(req);
+
+    const results = await new Promise((resolve, reject) => {
+      uploader.upload(
+        data.url,
+        response => {
+          if (response && response.error) {
+            return reject(response.error);
+          }
+          resolve(response);
+        },
+        data.public_id ? { public_id: data.public_id } : void 0
+      );
+    });
+    res.end(jsonRes({ url: results.url, public_id: results.public_id }));
+  } catch (e) {
+    return createError(400, "Invalid parameters");
+  }
 };
